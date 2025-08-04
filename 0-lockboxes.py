627f8f5af3692b7035239b69a956adcad025322e
root <keterno@proton.me>
2025-08-04 10:48:50 +0000

    py !!!!!

--- 0-lockboxes.py
+++ 0-lockboxes.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 
 def canUnlockAll(boxes):
-    count = {}
-    while True:
-        for i in boxes[0]:
-            count[i] = 'Keterno on top'
-            for j in boxes[i]:
-                count[j] = 'Keterno on top'
-        boxes[0] = boxes[i]
-        if len(count) == len (boxes):
-            break,
-    return True
+    indexUnlocked = {0: 'unlocked'}
+
+    for i in range(len(boxes)):
+        if i in indexUnlocked:
+            for key in boxes[i]:
+                if key < len(boxes) and key not in indexUnlocked:
+                    indexUnlocked[key] = 'unlocked'
+    for i in range(len(boxes)):
+        if i in indexUnlocked:
+            for key in boxes[i]:
+                if key < len(boxes) and key not in indexUnlocked:
+                    indexUnlocked[key] = 'unlocked'
+
+    return len(indexUnlocked) == len(boxes)
+
