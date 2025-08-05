35aa84eac00fdbfafaaefe51cb9a3828615bdcaf
root <keterno@proton.me>
2025-08-05 08:41:16 +0000

    05/08

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+"""
+Module to determine the minimum number of operations to reach a target number
+"""
+
+def minOperations(n):
+    if n <= 1:
+       return 0
+    operations = 0
+    aa = 'h'
+    oldaa = ''
+    while len(aa) < n:
+        if len(aa + oldaa) >= n:
+            operations += 1
+            break
+        oldaa = aa
+        aa += oldaa
+        operations += 2
+    return operations
+
