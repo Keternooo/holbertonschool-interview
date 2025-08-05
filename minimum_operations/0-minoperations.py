a2cba39e7c37cb4b8abe6d5a1114d6c3e5aaab2c
root <keterno@proton.me>
2025-08-05 09:10:21 +0000

    05/08

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -5,19 +5,12 @@ Module to determine the minimum number of operations to reach a target number
 
 def minOperations(n):
     if n <= 1:
-       return 0
+        return 0
     operations = 0
-    aa = 'h'
-    oldaa = ''
-    while len(aa) < n:
-        if len(aa + oldaa) == n:
-            operations += 1
-            break
-        elif len(aa + oldaa) > n:
-            operations += 2
-            break
-        oldaa = aa
-        aa += oldaa
-        operations += 2
-    return operations
 
+    for i in range(2, n + 1):
+        while n % i == 0:
+            operations += i
+            n = n / i
+
+    return operations
