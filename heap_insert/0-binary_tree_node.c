6f7bccfe464685f0c2e407e03adc6e5133fbc8d5
root <keterno@proton.me>
2025-08-08 13:08:40 +0000

    08/08

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -1,6 +1,12 @@
 #include "binary_trees.h"
 #include <stdlib.h>
 
+/**
+ * binary_tree_node: Create a tree or append some childs
+ * @parent: The tree to append
+ * @value: The value to append
+ * Return:  The new node or 0 (fail)
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode;
