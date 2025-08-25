b93e8428a27cc41c98de196027e0d5e291d528ce
xfc <keterno@proton.me>
2025-08-25 15:27:00 +0200

    Automatic push

---
+++ heap_insert/1-heap_insert.c
@@ -0,0 +1,63 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+void exchange_values(heap_t *a, heap_t *b)
+{
+	int temp = a->n;
+	a->n = b->n;
+	b->n = temp;
+}
+
+heap_t *find(heap_t *root)
+{
+	heap_t **queue, *node;
+	 size_t front = 0, back = 0;
+
+	queue = malloc(sizeof(*queue) * 1024);
+	if (!queue)
+		return (NULL);
+
+	queue[back++] = root;
+	while (front < back)
+	{
+		node = queue[front++];
+		if (!node->left || !node-right)
+		{
+			free(queue);
+			return (node);
+		}
+		queue[back++] = node->left;
+		queue[back++] = node->right;
+	}
+	free(queue);
+	return (NULL);
+}
+
+
+heap_t *heap_insert(heap_t **root, int value)
+{
+	heap_t *new, *parent;
+
+	if (*root == NULL)
+	{
+		*root = binary_tree_node(NULL, value);
+		return (*root);
+	}
+	parent = find(*root);
+	if (!parent)
+		reuturn (NULL);
+	new = binary_tree_node(parent, value);
+	if (!new)
+		return (NULL);
+	if (parent->left == NULL)
+		parent->left = new;
+	else
+		parent->right = new;
+
+	while (new->parent && new->n > new->parent->n)
+	{
+		exchange_values(new, new->parent);
+		new = new->parent;
+	}
+	return (new);
+}
